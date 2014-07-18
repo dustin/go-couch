3309510e374e6f434a04f5b7e49f375492de3236
Traun Leyden <tleyden@couchbase.com>
2014-07-18 13:34:52 -0700

    Fix test breakages introduced by 1c64a1611a6471c9928666749832c5ba510ec02d

--- couch_test.go
+++ couch_test.go
@@ -231,7 +231,7 @@ func TestUnmarshalBadReq(t *testing.T) {
 func TestRunningSuccess(t *testing.T) {
 	defer uninstallFakeHTTP(installFakeHTTP(oneFake(http.Response{
 		StatusCode: 200,
-		Body:       ioutil.NopCloser(strings.NewReader(`["adb"]`)),
+		Body:       ioutil.NopCloser(strings.NewReader(`{"version":"CouchDB"}`)),
 	})))
 	d := Database{}
 	if !d.Running() {
@@ -242,7 +242,7 @@ func TestRunningSuccess(t *testing.T) {
 func TestRunningEmpty(t *testing.T) {
 	defer uninstallFakeHTTP(installFakeHTTP(oneFake(http.Response{
 		StatusCode: 200,
-		Body:       ioutil.NopCloser(strings.NewReader(`[]`)),
+		Body:       ioutil.NopCloser(strings.NewReader(`{}`)),
 	})))
 	d := Database{}
 	if d.Running() {
@@ -897,7 +897,7 @@ func TestNewDBCreateExists(t *testing.T) {
 		responses: []http.Response{
 			http.Response{
 				StatusCode: 200,
-				Body:       ioutil.NopCloser(strings.NewReader(`["x"]`)),
+				Body:       ioutil.NopCloser(strings.NewReader(`{"version":"CouchDB"}`)),
 			},
 			http.Response{
 				StatusCode: 200,
@@ -916,7 +916,7 @@ func TestNewDBCreateSuccess(t *testing.T) {
 		responses: []http.Response{
 			http.Response{
 				StatusCode: 200,
-				Body:       ioutil.NopCloser(strings.NewReader(`["x"]`)),
+				Body:       ioutil.NopCloser(strings.NewReader(`{"version":"CouchDB"}`)),
 			},
 			http.Response{
 				StatusCode: 404,
@@ -1067,7 +1067,7 @@ func TestConnectSuccess(t *testing.T) {
 		responses: []http.Response{
 			http.Response{
 				StatusCode: 200,
-				Body:       ioutil.NopCloser(strings.NewReader(`["db"]`)),
+				Body:       ioutil.NopCloser(strings.NewReader(`{"version":"CouchDB"}`)),
 			},
 			http.Response{
 				StatusCode: 200,
@@ -1090,7 +1090,7 @@ func TestConnectSuccessDefaultPort(t *testing.T) {
 		responses: []http.Response{
 			http.Response{
 				StatusCode: 200,
-				Body:       ioutil.NopCloser(strings.NewReader(`["db"]`)),
+				Body:       ioutil.NopCloser(strings.NewReader(`{"version":"CouchDB"}`)),
 			},
 			http.Response{
 				StatusCode: 200,
